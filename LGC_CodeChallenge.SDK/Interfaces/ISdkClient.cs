99c2f93eb3267e5bfafd1804561eb9ae47661e8e
Pedro Tavares <1180582@isep.ipp.pt>
2024-12-11 02:20:50 +0000

    Added the sdk project Added the contracts Added the Project where will be the tests for the LGC_CodeChallenge API

---
+++ LGC_CodeChallenge.SDK/Interfaces/ISdkClient.cs
@@ -0,0 +1,19 @@
+﻿using LGC_Code_Challenge.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGC_CodeChallenge.SDK.Interfaces
+{
+    public interface ISdkClient
+    {
+        Task<TResponse> GetAsync<TResponse>(string endpoint);
+        Task<List<TResponse>> GetAllAsync<TResponse>(string endpoint);
+        Task<TResponse> PostAsync<TRequest, TResponse>(string endpoint, TRequest request);
+        Task<TResponse> PutAsync<TRequest, TResponse>(string endpoint,TRequest request);
+        Task DeleteAsync(string endpoint);
+
+    }
+}
